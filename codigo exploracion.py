773a826224ea0732bda8eeb0ecd40c3e6cfceb38
daniforeroolano <d.forero@uniandes.edu.co>
2023-09-27 16:10:55 -0500

    Actualizar codigo exploracion.py con cambios importantes

--- codigo exploracion.py
+++ codigo exploracion.py
@@ -1,34 +1,63 @@
+from ucimlrepo import fetch_ucirepo 
 import pandas as pd 
 import matplotlib.pyplot as plt
-df = pd.read_csv('C:/Users/oem/Documents/universidad de los andes/octavo/Analitica computacional para la toma de decisiones/Proyecto 1/data.csv', sep=';')
+#importo los datos
+dataset = fetch_ucirepo(id=697)
+#Separo para que no me quede una tabla
+data = dataset['data']
+targets = dataset['data']['targets']
 
+# Convierto a dataframe
+df_data = pd.DataFrame(data['features'])
+
+# meto el target al data frame
+df = pd.concat([df_data, pd.DataFrame({'Target': targets['Target']})], axis=1)
+
+# Especificar las variables que queremos discretizar
+variables_a_discretizar = ['Previous qualification (grade)', 'Admission grade','Unemployment rate','Inflation rate','GDP']
+
+# Especificicar el número de intervalos deseados para cada variable
+num_bins = 4
+
+# Crear un nuevo DataFrame para almacenar las variables originales y discretizadas
+df_nuevo = pd.DataFrame()
+# Discretizar las variables seleccionadas y añadirlas al nuevo DataFrame
+for col in variables_a_discretizar:
+    df[col+'_bin'] = pd.cut(df[col], bins=num_bins, labels=False)
+    df_nuevo[col+'_bin'] = df[col+'_bin']
+
+# Añadir las variables que no se han discretizado al nuevo DataFrame
+for col in df.columns:
+    if col not in variables_a_discretizar and 'bin' not in col:
+        df_nuevo[col] = df[col]
+
+# Obtener los límites de los intervalos para cada variable
+bin_limits = {col: pd.cut(df[col], bins=num_bins).unique() for col in variables_a_discretizar}
+# Imprimir el nuevo DataFrame con las variables originales y discretizadas
+print(df_nuevo)
+print("Bin limits:")
+print(bin_limits)
+
+# Exploracion inicial
 print("columns")
-print(df.columns) 
+print(df_nuevo.columns) 
 print("size")
-print(df.size) 
+print(df_nuevo.size) 
 print("info")
-print(df.info())
+print(df_nuevo.info())
 print("shape")
-print(df.shape)
+print(df_nuevo.shape)
 print("describe")
-print(df.describe)
+print(df_nuevo.describe)
 print("nulos")
-print(df.isnull().sum())
-print("unicos")
-for columna in df.columns:
-    print(f'Valores unicos en la columna: {columna}: ')
-    print(df[columna].unique())
-    print('\n')
+print(df_nuevo.isnull().sum())
 
-    
 print("Grafico del target")
 # Calcular la frecuencia de cada valor en la columna "target"
-frecuencia_target = df['Target'].value_counts()
+frecuencia_target = df_nuevo['Target'].value_counts()
 
 # Crear el gráfico de frecuencia
 plt.bar(frecuencia_target.index, frecuencia_target.values)
-
-# Personalizar el gráfico
 plt.xlabel('Valor')
 plt.ylabel('Frecuencia')
 plt.title('Frecuencia de la columna "target"')
@@ -36,3 +65,15 @@ plt.xticks(rotation=45)  # Rotar las etiquetas del eje x para mayor legibilidad
 
 # Mostrar el gráfico
 plt.show()
+#Comparar el comportamiento de cada uno de las variables contra la objetivo
+for feature in df_nuevo.columns:
+    if feature != 'Target':
+        plt.figure(figsize=(10, 6))
+        df_nuevo.boxplot(column=feature, by='Target', ax=plt.gca())
+        plt.title(f'Boxplot of {feature} by Target')
+        plt.suptitle('')  # Suppress the default title
+        plt.xlabel('Target')
+        plt.ylabel(feature)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
